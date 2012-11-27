65658e57d8886b84e97edcf7976b47cd1090f34e
vkaldi <vedran@vkaldi.com>
2012-11-27 16:15:02 +0100

    enabled non-reentrant tasks

--- ConsoleTester/Program.cs
+++ ConsoleTester/Program.cs
@@ -6,58 +6,72 @@ using FluentScheduler.Model;
 
 namespace ConsoleTester
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			Console.WriteLine("Starting everything...");
-			TaskManager.Initialize(new MyRegistry());
-			Console.WriteLine("Done initializing...");
-			//Thread.Sleep(10000);
-			//TaskManager.Stop();
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Test();
+            MultithreadingTest();
+            Console.ReadKey();
+        }
 
-/*			TaskManager.AddTask(() => Console.WriteLine("Inline task: " + DateTime.Now), x => x.ToRunEvery(15).Seconds());
-			TaskManager.AddTask(() => Console.WriteLine("Inline task (once): " + DateTime.Now), x => x.ToRunOnceAt(DateTime.Now.AddSeconds(5)));
+        static void TaskManager_UnobservedTaskException(Task sender, UnhandledExceptionEventArgs e)
+        {
+            Console.WriteLine("Something went wrong: " + e.ExceptionObject);
+        }
 
-			TaskManager.AddTask<MyInlineTask>(x => x.ToRunNow());
-*/
-			TaskManager.UnobservedTaskException += TaskManager_UnobservedTaskException;
-/*			TaskManager.AddTask(() => {
-			                          	Console.WriteLine("Inline task: " + DateTime.Now); 
-				throw new Exception("Hi"); }, x => x.ToRunNow());
-*/
-			Console.ReadKey();
-		}
+        static void Test()
+        {
+            Console.WriteLine("Starting everything...");
+            TaskManager.Initialize(new MyRegistry());
+            Console.WriteLine("Done initializing...");
+            //Thread.Sleep(10000);
+            //TaskManager.Stop();
 
-		static void TaskManager_UnobservedTaskException(Task sender, UnhandledExceptionEventArgs e)
-		{
-			Console.WriteLine("Something went wrong: " + e.ExceptionObject);
-		}
-	}
+            /*			TaskManager.AddTask(() => Console.WriteLine("Inline task: " + DateTime.Now), x => x.ToRunEvery(15).Seconds());
+                        TaskManager.AddTask(() => Console.WriteLine("Inline task (once): " + DateTime.Now), x => x.ToRunOnceAt(DateTime.Now.AddSeconds(5)));
 
-	public class MyRegistry : Registry
-	{
-		public MyRegistry()
-		{
-			Schedule(() =>
-			{
-				Console.WriteLine(DateTime.Now.ToString());
-			}).ToRunEvery(0).Weeks().On(DayOfWeek.Sunday).At(15, 24);
-		}
-	}
+                        TaskManager.AddTask<MyInlineTask>(x => x.ToRunNow());
+            */
+            TaskManager.UnobservedTaskException += TaskManager_UnobservedTaskException;
+            /*			TaskManager.AddTask(() => {
+                                                    Console.WriteLine("Inline task: " + DateTime.Now); 
+                            throw new Exception("Hi"); }, x => x.ToRunNow());
+            */
+        }
 
-	public class MyTask : ITask
-	{
-		public void Execute()
-		{
-			Console.WriteLine("ITask Task: " + DateTime.Now);
-		}
-	}
-	public class MyInlineTask : ITask
-	{
-		public void Execute()
-		{
-			Console.WriteLine("ITask Inline Task: " + DateTime.Now);
-		}
-	}
+        static void MultithreadingTest()
+        {
+            Console.WriteLine("Starting multithreading test...");
+
+
+            TaskManager.Initialize(new MultithreadingSchedule());
+        }
+    }
+
+    public class MyRegistry : Registry
+    {
+        public MyRegistry()
+        {
+            Schedule(() =>
+            {
+                Console.WriteLine(DateTime.Now.ToString());
+            }).ToRunEvery(0).Weeks().On(DayOfWeek.Sunday).At(15, 24);
+        }
+    }
+
+    public class MyTask : ITask
+    {
+        public void Execute()
+        {
+            Console.WriteLine("ITask Task: " + DateTime.Now);
+        }
+    }
+    public class MyInlineTask : ITask
+    {
+        public void Execute()
+        {
+            Console.WriteLine("ITask Inline Task: " + DateTime.Now);
+        }
+    }
 }
--- FluentScheduler/TaskManager.cs
+++ FluentScheduler/TaskManager.cs
@@ -7,190 +7,237 @@ using FluentScheduler.Model;
 
 namespace FluentScheduler
 {
-	/// <summary>
-	/// Controls the timer logic to execute all configured tasks.
-	/// </summary>
-	public static class TaskManager
-	{
-		public static event GenericEventHandler<Task, UnhandledExceptionEventArgs> UnobservedTaskException;
-		private static List<Schedule> _tasks;
-		private static Timer _timer;
+    /// <summary>
+    /// Controls the timer logic to execute all configured tasks.
+    /// </summary>
+    public static class TaskManager
+    {
+        public static event GenericEventHandler<Task, UnhandledExceptionEventArgs> UnobservedTaskException;
+        private static List<Schedule> _tasks;
+        private static List<Action> _runningTasks;
+        private static Timer _timer;
 
-		/// <summary>
-		/// Initializes the task manager with all schedules configured in the specified registry
-		/// </summary>
-		/// <param name="registry">Registry containing task schedules</param>
-		public static void Initialize(Registry registry)
-		{
-			var immediateTasks = new List<Schedule>();
-			lock (typeof(TaskManager))
-			{
-				var now = DateTime.Now;
-				_tasks = new List<Schedule>();
+        /// <summary>
+        /// Initializes the task manager with all schedules configured in the specified registry
+        /// </summary>
+        /// <param name="registry">Registry containing task schedules</param>
+        public static void Initialize(Registry registry)
+        {
+            var immediateTasks = new List<Schedule>();
+            lock (typeof(TaskManager))
+            {
+                var now = DateTime.Now;
+                _tasks = new List<Schedule>();
+                _runningTasks = new List<Action>();
 
-				AddSchedules(registry.Schedules, immediateTasks, now);
-			}
+                AddSchedules(registry.Schedules, immediateTasks, now);
+            }
 
-			RunAndInitializeSchedule(immediateTasks);
-		}
+            RunAndInitializeSchedule(immediateTasks);
+        }
 
-		private static void ThrowUnobservedTaskException(Task t)
-		{
-			var handler = UnobservedTaskException;
-			if (handler != null)
-				handler(t, new UnhandledExceptionEventArgs(t.Exception.InnerException, true));
-		}
+        private static void ThrowUnobservedTaskException(Task t)
+        {
+            var handler = UnobservedTaskException;
+            if (handler != null)
+                handler(t, new UnhandledExceptionEventArgs(t.Exception.InnerException, true));
+        }
 
-		private static void AddSchedules(IEnumerable<Schedule> schedules, ICollection<Schedule> immediateTasks, DateTime now)
-		{
-			foreach (var schedule in schedules)
-			{
-				if (schedule.CalculateNextRun == null)
-				{
-					immediateTasks.Add(schedule);
-				}
-				else
-				{
-					schedule.NextRunTime = schedule.CalculateNextRun(now);
-					_tasks.Add(schedule);
-				}
+        private static void AddSchedules(IEnumerable<Schedule> schedules, ICollection<Schedule> immediateTasks, DateTime now)
+        {
+            foreach (var schedule in schedules)
+            {
+                if (schedule.CalculateNextRun == null)
+                {
+                    immediateTasks.Add(schedule);
+                }
+                else
+                {
+                    schedule.NextRunTime = schedule.CalculateNextRun(now);
+                    _tasks.Add(schedule);
+                }
 
-				foreach (var childSchedule in schedule.AdditionalSchedules)
-				{
-					if (childSchedule.CalculateNextRun == null)
-					{
-						immediateTasks.Add(childSchedule);
-						continue;
-					}
-					childSchedule.NextRunTime = childSchedule.CalculateNextRun(now);
-					_tasks.Add(childSchedule);
-				}
-			}
-		}
+                foreach (var childSchedule in schedule.AdditionalSchedules)
+                {
+                    if (childSchedule.CalculateNextRun == null)
+                    {
+                        immediateTasks.Add(childSchedule);
+                        continue;
+                    }
+                    childSchedule.NextRunTime = childSchedule.CalculateNextRun(now);
+                    _tasks.Add(childSchedule);
+                }
+            }
+        }
 
-		private static void RunAndInitializeSchedule(IEnumerable<Schedule> immediateTasks)
-		{
-			foreach (var task in immediateTasks)
-			{
-				Task.Factory.StartNew(task.Task, TaskCreationOptions.PreferFairness)
-					.ContinueWith(ThrowUnobservedTaskException, TaskContinuationOptions.OnlyOnFaulted);
-			}
+        private static void RunAndInitializeSchedule(IEnumerable<Schedule> immediateTasks)
+        {
+            foreach (var task in immediateTasks)
+            {
+                StartTask(task);
+            }
 
-			if (!_tasks.Any())
-				return;
+            if (!_tasks.Any())
+                return;
 
-			if (_timer == null)
-			{
-				_timer = new Timer { AutoReset = false };
-				_timer.Elapsed += Timer_Elapsed;
-			}
-			_tasks.Sort((x, y) => DateTime.Compare(x.NextRunTime, y.NextRunTime));
-			Schedule();
-		}
+            if (_timer == null)
+            {
+                _timer = new Timer { AutoReset = false };
+                _timer.Elapsed += Timer_Elapsed;
+            }
+            _tasks.Sort((x, y) => DateTime.Compare(x.NextRunTime, y.NextRunTime));
+            Schedule();
+        }
 
-		/// <summary>
-		/// Adds a task to the task manager
-		/// </summary>
-		/// <typeparam name="T">Task to schedule</typeparam>
-		/// <param name="taskSchedule">Schedule for the task</param>
-		public static void AddTask<T>(Action<Schedule> taskSchedule) where T : ITask, new()
-		{
-			if (taskSchedule == null)
-				throw new ArgumentNullException("taskSchedule", "Please specify the task schedule to add to the task manager.");
+        /// <summary>
+        /// Adds a task to the task manager
+        /// </summary>
+        /// <typeparam name="T">Task to schedule</typeparam>
+        /// <param name="taskSchedule">Schedule for the task</param>
+        public static void AddTask<T>(Action<Schedule> taskSchedule) where T : ITask, new()
+        {
+            if (taskSchedule == null)
+                throw new ArgumentNullException("taskSchedule", "Please specify the task schedule to add to the task manager.");
 
-			var schedule = new Schedule(new T());
-			AddTask(taskSchedule, schedule);
-		}
+            var schedule = new Schedule(new T());
+            AddTask(taskSchedule, schedule);
+        }
 
-		/// <summary>
-		/// Adds a task to the task manager
-		/// </summary>
-		/// <param name="taskAction">Task to schedule</param>
-		/// <param name="taskSchedule">Schedule for the task</param>
-		public static void AddTask(Action taskAction, Action<Schedule> taskSchedule)
-		{
-			if (taskSchedule == null)
-				throw new ArgumentNullException("taskSchedule", "Please specify the task schedule to add to the task manager.");
+        /// <summary>
+        /// Adds a task to the task manager
+        /// </summary>
+        /// <param name="taskAction">Task to schedule</param>
+        /// <param name="taskSchedule">Schedule for the task</param>
+        public static void AddTask(Action taskAction, Action<Schedule> taskSchedule)
+        {
+            if (taskSchedule == null)
+                throw new ArgumentNullException("taskSchedule", "Please specify the task schedule to add to the task manager.");
 
-			var schedule = new Schedule(taskAction);
-			AddTask(taskSchedule, schedule);
-		}
+            var schedule = new Schedule(taskAction);
+            AddTask(taskSchedule, schedule);
+        }
 
-		private static void AddTask(Action<Schedule> taskSchedule, Schedule schedule)
-		{
-			taskSchedule(schedule);
+        private static void AddTask(Action<Schedule> taskSchedule, Schedule schedule)
+        {
+            taskSchedule(schedule);
 
-			var immediateTasks = new List<Schedule>();
-			lock (typeof(TaskManager))
-			{
-				var now = DateTime.Now;
-				if (_tasks == null)
-					_tasks = new List<Schedule>();
+            var immediateTasks = new List<Schedule>();
+            lock (typeof(TaskManager))
+            {
+                var now = DateTime.Now;
+                if (_tasks == null)
+                    _tasks = new List<Schedule>();
 
-				AddSchedules(new List<Schedule> { schedule }, immediateTasks, now);
-			}
+                AddSchedules(new List<Schedule> { schedule }, immediateTasks, now);
+            }
 
-			RunAndInitializeSchedule(immediateTasks);
-		}
+            RunAndInitializeSchedule(immediateTasks);
+        }
 
-		/// <summary>
-		/// Stops the task manager from executing tasks.
-		/// </summary>
-		public static void Stop()
-		{
-			if (_timer != null)
-				_timer.Stop();
-		}
+        /// <summary>
+        /// Stops the task manager from executing tasks.
+        /// </summary>
+        public static void Stop()
+        {
+            if (_timer != null)
+                _timer.Stop();
+        }
 
-		/// <summary>
-		/// Restarts the task manager if it had previously been stopped
-		/// </summary>
-		public static void Start()
-		{
-			if (_timer != null)
-				Schedule();
-		}
+        /// <summary>
+        /// Restarts the task manager if it had previously been stopped
+        /// </summary>
+        public static void Start()
+        {
+            if (_timer != null)
+                Schedule();
+        }
 
-		static void Timer_Elapsed(object sender, ElapsedEventArgs e)
-		{
-			Schedule();
-		}
+        static void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Schedule();
+        }
 
-		private static void Schedule()
-		{
-			_timer.Stop();
+        private static void Schedule()
+        {
+            _timer.Stop();
 
-			var firstTask = _tasks.First();
-			if (firstTask.NextRunTime <= DateTime.Now)
-			{
-				Task.Factory.StartNew(firstTask.Task, TaskCreationOptions.PreferFairness)
-					.ContinueWith(ThrowUnobservedTaskException, TaskContinuationOptions.OnlyOnFaulted);
-				firstTask.NextRunTime = firstTask.CalculateNextRun(DateTime.Now.AddMilliseconds(1));
-				if (firstTask.TaskExecutions > 0)
-				{
-					firstTask.TaskExecutions--;
-				}
-				if (firstTask.TaskExecutions == 0)
-				{
-					lock (typeof(TaskManager))
-					{
-						_tasks.Remove(firstTask);
-					}
-				}
-				_tasks.Sort((x, y) => DateTime.Compare(x.NextRunTime, y.NextRunTime));
-				Schedule();
-				return;
-			}
+            var firstTask = _tasks.First();
+            if (firstTask.NextRunTime <= DateTime.Now)
+            {
+                StartTask(firstTask);
 
-			var timerInterval = (firstTask.NextRunTime - DateTime.Now).TotalMilliseconds;
-			if (timerInterval <= 0)
-			{
-				Schedule();
-				return;
-			}
+                firstTask.NextRunTime = firstTask.CalculateNextRun(DateTime.Now.AddMilliseconds(1));
+                if (firstTask.TaskExecutions > 0)
+                {
+                    firstTask.TaskExecutions--;
+                }
+                if (firstTask.TaskExecutions == 0)
+                {
+                    lock (typeof(TaskManager))
+                    {
+                        _tasks.Remove(firstTask);
+                    }
+                }
+                _tasks.Sort((x, y) => DateTime.Compare(x.NextRunTime, y.NextRunTime));
+                Schedule();
+                return;
+            }
 
-			_timer.Interval = timerInterval;
-			_timer.Start();
-		}
-	}
+            var timerInterval = (firstTask.NextRunTime - DateTime.Now).TotalMilliseconds;
+            if (timerInterval <= 0)
+            {
+                Schedule();
+                return;
+            }
+
+            _timer.Interval = timerInterval;
+            _timer.Start();
+        }
+
+        /// <summary>
+        /// Start new task.
+        /// Only reentrant tasks (<see cref="Schedule.IsReentrant"/>) can be started if they are already running.
+        /// If task is already running and IsReentrant is false, than we are not doing anything.
+        /// </summary>
+        /// <param name="task"></param>
+        private static void StartTask(Schedule task)
+        {
+            if (!task.IsReentrant)
+            {
+                // if task does not support reentry, we need to check that another instance is not already executing
+                lock (typeof(TaskManager))
+                {
+                    if (_runningTasks.Contains(task.Task))
+                    {
+                        // task is already running, nothing to do here :)
+                        return;
+                    }
+                }
+            }
+
+            Task.Factory.StartNew(() =>
+            {
+                try
+                {
+                    lock (typeof(TaskManager))
+                    {
+                        // add to running tasks
+                        _runningTasks.Add(task.Task);
+                    }
+                    // execute task
+                    task.Task();
+                }
+                finally
+                {
+                    // remove from running tasks
+                    lock (typeof(TaskManager))
+                    {
+                        _runningTasks.Remove(task.Task);
+                    }
+                }
+            }
+                , TaskCreationOptions.PreferFairness)
+                    .ContinueWith(ThrowUnobservedTaskException, TaskContinuationOptions.OnlyOnFaulted);
+        }
+    }
 }
