a8df241888d21335b976035f2e51732ab6c6bd6f
vkaldi <git@vkaldi.com>
2013-02-18 13:41:49 +0100

    Added the ability to get a schedule info based on the schedule's name

--- FluentScheduler/Model/Schedule.cs
+++ FluentScheduler/Model/Schedule.cs
@@ -3,102 +3,111 @@ using System.Collections.Generic;
 
 namespace FluentScheduler.Model
 {
-	public class Schedule
-	{
-		public DateTime NextRunTime { get; set; }
-		public string Name { get; set; }
+    public class Schedule
+    {
+        public DateTime NextRunTime { get; set; }
+        public string Name { get; set; }
 
-		internal Action Task { get; private set; }
+        internal Action Task { get; private set; }
 
-		internal Func<DateTime, DateTime> CalculateNextRun { get; set; }
+        internal Func<DateTime, DateTime> CalculateNextRun { get; set; }
 
-		internal ICollection<Schedule> AdditionalSchedules { get; set; }
-		internal bool Reentrant { get; set; }
-		internal Schedule Parent { get; set; }
-		internal int TaskExecutions { get; set; }
+        internal ICollection<Schedule> AdditionalSchedules { get; set; }
+        internal bool Reentrant { get; set; }
+        internal Schedule Parent { get; set; }
+        internal int TaskExecutions { get; set; }
 
-		/// <summary>
-		/// Schedules the specified task to run
-		/// </summary>
-		/// <param name="task">Task to run</param>
-		public Schedule(ITask task) : this(task.Execute)
-		{
-		}
+        /// <summary>
+        /// Schedules the specified task to run
+        /// </summary>
+        /// <param name="task">Task to run</param>
+        public Schedule(ITask task)
+            : this(task.Execute)
+        {
+        }
 
-		/// <summary>
-		/// Schedules the specified task to run
-		/// </summary>
-		/// <param name="action">Task to run</param>
-		public Schedule(Action action)
-		{
-			Task = action;
-			AdditionalSchedules = new List<Schedule>();
-			TaskExecutions = -1;
-			Reentrant = true;
-		}
+        /// <summary>
+        /// Schedules the specified task to run
+        /// </summary>
+        /// <param name="action">Task to run</param>
+        public Schedule(Action action)
+        {
+            Task = action;
+            AdditionalSchedules = new List<Schedule>();
+            TaskExecutions = -1;
+            Reentrant = true;
+        }
 
-		/// <summary>
-		/// Schedules the specified task to run now
-		/// </summary>
-		/// <returns></returns>
-		public SpecificRunTime ToRunNow()
-		{
-			return new SpecificRunTime(this);
-		}
+        /// <summary>
+        /// Start the task now, regardless of any scheduled start time.
+        /// </summary>
+        public void Execute()
+        {
+            TaskManager.StartTask(this);
+        }
 
-		/// <summary>
-		/// Schedules the specified task to run for the specified interval
-		/// </summary>
-		/// <param name="interval"></param>
-		/// <returns></returns>
-		public TimeUnit ToRunEvery(int interval)
-		{
-			return new TimeUnit(this, interval);
-		}
+        /// <summary>
+        /// Schedules the specified task to run now
+        /// </summary>
+        /// <returns></returns>
+        public SpecificRunTime ToRunNow()
+        {
+            return new SpecificRunTime(this);
+        }
 
-		/// <summary>
-		/// Schedules the specified task to run once at the hour and minute specified.  If the hour and minute have passed, the task will be executed immediately.
-		/// </summary>
-		/// <param name="hours">0-23: Represents the hour of today</param>
-		/// <param name="minutes">0-59: Represents the minute to run the task</param>
-		/// <returns></returns>
-		public SpecificRunTime ToRunOnceAt(int hours, int minutes)
-		{
-			return ToRunOnceAt(new DateTime(DateTime.Today.Year, DateTime.Today.Month, DateTime.Today.Day, hours, minutes, 0));
-		}
+        /// <summary>
+        /// Schedules the specified task to run for the specified interval
+        /// </summary>
+        /// <param name="interval"></param>
+        /// <returns></returns>
+        public TimeUnit ToRunEvery(int interval)
+        {
+            return new TimeUnit(this, interval);
+        }
 
-		/// <summary>
-		/// Schedules the specified task to run once at the time specified.  If the time has passed, the task will be executed immediately.
-		/// </summary>
-		/// <param name="time">Time to run the task</param>
-		/// <returns></returns>
-		public SpecificRunTime ToRunOnceAt(DateTime time)
-		{
-			CalculateNextRun = x => time;
-			TaskExecutions = 1;
+        /// <summary>
+        /// Schedules the specified task to run once at the hour and minute specified.  If the hour and minute have passed, the task will be executed immediately.
+        /// </summary>
+        /// <param name="hours">0-23: Represents the hour of today</param>
+        /// <param name="minutes">0-59: Represents the minute to run the task</param>
+        /// <returns></returns>
+        public SpecificRunTime ToRunOnceAt(int hours, int minutes)
+        {
+            return ToRunOnceAt(new DateTime(DateTime.Today.Year, DateTime.Today.Month, DateTime.Today.Day, hours, minutes, 0));
+        }
 
-			return new SpecificRunTime(this);
-		}
+        /// <summary>
+        /// Schedules the specified task to run once at the time specified.  If the time has passed, the task will be executed immediately.
+        /// </summary>
+        /// <param name="time">Time to run the task</param>
+        /// <returns></returns>
+        public SpecificRunTime ToRunOnceAt(DateTime time)
+        {
+            CalculateNextRun = x => time;
+            TaskExecutions = 1;
 
-		/// <summary>
-		/// Provide a name for this schedule
-		/// </summary>
-		/// <param name="name">Name of this schedule</param>
-		/// <returns></returns>
-		public Schedule WithName(string name)
-		{
-			Name = name;
-			return this;
-		}
+            return new SpecificRunTime(this);
+        }
 
-		/// <summary>
-		/// Will not start a new instance of the task if a previous schedule is still running
-		/// </summary>
-		/// <returns></returns>
-		public Schedule NonReentrant()
-		{
-			Reentrant = false;
-			return this;
-		}
-	}
+        /// <summary>
+        /// Provide a name for this schedule
+        /// </summary>
+        /// <param name="name">Name of this schedule</param>
+        /// <returns></returns>
+        public Schedule WithName(string name)
+        {
+            Name = name;
+            return this;
+        }
+
+        /// <summary>
+        /// Will not start a new instance of the task if a previous schedule is still running
+        /// </summary>
+        /// <returns></returns>
+        public Schedule NonReentrant()
+        {
+            Reentrant = false;
+            return this;
+        }
+    }
 }
