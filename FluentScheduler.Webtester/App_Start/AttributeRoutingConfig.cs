faa1a1cb80e56e45b20dbd94411305c0e3b25a4b
Jim Geurts <jim@biacreations.com>
2013-05-24 09:42:07 -0500

    Cleanup nuget creation Updated nugets used in the solution

--- FluentScheduler.Webtester/App_Start/AttributeRoutingConfig.cs
+++ FluentScheduler.Webtester/App_Start/AttributeRoutingConfig.cs
@@ -1,19 +1,22 @@
 using System.Web.Routing;
 using AttributeRouting.Web.Mvc;
 
-[assembly: WebActivator.PreApplicationStartMethod(typeof(FluentScheduler.Webtester.App_Start.AttributeRouting), "Start")]
+[assembly: WebActivator.PreApplicationStartMethod(typeof(FluentScheduler.Webtester.AttributeRoutingConfig), "Start")]
 
-namespace FluentScheduler.Webtester.App_Start {
-    public static class AttributeRouting {
-		public static void RegisterRoutes(RouteCollection routes) {
-            
+namespace FluentScheduler.Webtester 
+{
+    public static class AttributeRoutingConfig
+	{
+		public static void RegisterRoutes(RouteCollection routes) 
+		{    
 			// See http://github.com/mccalltd/AttributeRouting/wiki for more options.
 			// To debug routes locally using the built in ASP.NET development server, go to /routes.axd
             
 			routes.MapAttributeRoutes();
 		}
 
-        public static void Start() {
+        public static void Start() 
+		{
             RegisterRoutes(RouteTable.Routes);
         }
     }
