d939f18cc02da651f17dd63c96b6b4424bf2179e
Talles L <talleslasmar@gmail.com>
2016-03-17 13:15:21 -0300

    Updated the test application log library

--- TestApplication/MyRegistry.cs
+++ TestApplication/MyRegistry.cs
@@ -1,6 +1,6 @@
 ﻿namespace FluentScheduler.Tests.TestApplication
 {
-    using LogThis;
+    using LLibrary;
     using System;
     using System.Threading;
 
@@ -36,109 +36,109 @@
                 .AndThen(() => Console.Write("1, "))
                 .AndThen(() => Console.WriteLine("Live!"))
                 .AndThen(() => Console.WriteLine("{0}You can check what's happening in the log file at \"{1}\"",
-                    Environment.NewLine, Log.Directory));
+                    Environment.NewLine, L.Directory));
         }
         private void NonReentrant()
         {
-            Log.Register("[non reentrant]");
+            L.Register("[non reentrant]");
 
             Schedule(() =>
             {
-                Log.This("[non reentrant]", "Sleeping a minute...");
+                L.Log("[non reentrant]", "Sleeping a minute...");
                 Thread.Sleep(TimeSpan.FromMinutes(1));
             }).NonReentrant().WithName("[non reentrant]").ToRunEvery(1).Seconds();
         }
 
         private void Reentrant()
         {
-            Log.Register("[reentrant]");
+            L.Register("[reentrant]");
 
             Schedule(() =>
             {
-                Log.This("[reentrant]", "Sleeping a minute...");
+                L.Log("[reentrant]", "Sleeping a minute...");
                 Thread.Sleep(TimeSpan.FromMinutes(3));
             }).WithName("[reentrant]").ToRunNow().AndEvery(1).Minutes();
         }
 
         private void OnceIn()
         {
-            Log.Register("[once in]");
+            L.Register("[once in]");
 
             Schedule(() =>
             {
                 JobManager.RemoveJob("[reentrant]");
                 JobManager.RemoveJob("[non reentrant]");
-                Log.This("[once in]", "Disabled the reentrant and non reentrant jobs.");
+                L.Log("[once in]", "Disabled the reentrant and non reentrant jobs.");
             }).WithName("[once in]").ToRunOnceIn(3).Minutes();
         }
 
         private void OnceAt()
         {
-            Log.Register("[once at]");
+            L.Register("[once at]");
 
-            Schedule(() => Log.This("[once at]", "It's almost midnight."))
+            Schedule(() => L.Log("[once at]", "It's almost midnight."))
                 .WithName("[once at]").ToRunOnceAt(23, 50);
         }
 
         private void TenMinutes()
         {
-            Log.Register("[ten minutes]");
+            L.Register("[ten minutes]");
 
-            Schedule(() => Log.This("[ten minutes]", "Ten minutes has passed."))
+            Schedule(() => L.Log("[ten minutes]", "Ten minutes has passed."))
                 .WithName("[ten minutes]").ToRunEvery(10).Minutes();
         }
 
         private void Sleepy()
         {
-            Log.Register("[sleepy]");
+            L.Register("[sleepy]");
 
             Schedule(() =>
             {
-                Log.This("[sleepy]", "Sleeping...");
+                L.Log("[sleepy]", "Sleeping...");
                 Thread.Sleep(new TimeSpan(0, 7, 30));
             }).WithName("[sleepy]").ToRunEvery(15).Minutes();
         }
 
         private void Faulty()
         {
-            Log.Register("[faulty]");
+            L.Register("[faulty]");
 
             Schedule(() =>
             {
-                Log.This("[faulty]", "I'm going to raise an exception!");
+                L.Register("[faulty]", "I'm going to raise an exception!");
                 throw new Exception("I warned you.");
             }).WithName("[faulty]").ToRunEvery(20).Minutes();
         }
 
         private void Hour()
         {
-            Log.Register("[hour]");
+            L.Register("[hour]");
 
-            Schedule(() => Log.This("[hour]", "A hour has passed."))
+            Schedule(() => L.Log("[hour]", "A hour has passed."))
                 .WithName("[hour]").ToRunEvery(1).Hours();
         }
 
         private void Day()
         {
-            Log.Register("[day]");
+            L.Register("[day]");
 
-            Schedule(() => Log.This("[day]", "A day has passed."))
+            Schedule(() => L.Log("[day]", "A day has passed."))
                 .WithName("[day]").ToRunEvery(1).Days();
         }
 
         private void Weekday()
         {
-            Log.Register("[weekday]");
+            L.Register("[weekday]");
 
-            Schedule(() => Log.This("[weekday]", "A new weekday has started."))
+            Schedule(() => L.Log("[weekday]", "A new weekday has started."))
                 .WithName("[weekday]").ToRunEvery(1).Weekdays();
         }
 
         private void Week()
         {
-            Log.Register("[week]");
+            L.Register("[week]");
 
-            Schedule(() => Log.This("[week]", "A new week has started."))
+            Schedule(() => L.Log("[week]", "A new week has started."))
                 .WithName("[week]").ToRunEvery(1).Weeks();
         }
     }
--- TestApplication/Program.cs
+++ TestApplication/Program.cs
@@ -1,6 +1,6 @@
 ﻿namespace FluentScheduler.Tests.TestApplication
 {
-    using LogThis;
+    using LLibrary;
     using System;
     using System.Threading;
 
@@ -17,24 +17,24 @@
 
         private static void ListenForStart()
         {
-            Log.Register("[job start]", "\"{0}\" has started.");
-            JobManager.JobStart += (schedule, e) => Log.This("[job start]", schedule.Name);
+            L.Register("[job start]", "\"{0}\" has started.");
+            JobManager.JobStart += (schedule, e) => L.Log("[job start]", schedule.Name);
         }
 
         private static void ListenForEnd()
         {
-            Log.Register("[job end]", "\"{0}\" has ended{1}.");
+            L.Register("[job end]", "\"{0}\" has ended{1}.");
 
             JobManager.JobEnd += (schedule, e) =>
-                Log.This("[job end]", schedule.Name,
+                L.Log("[job end]", schedule.Name,
                     schedule.Duration > TimeSpan.FromSeconds(1) ?
                     " with duration of " + schedule.Duration : string.Empty);
         }
 
         private static void ListenForException()
         {
-            Log.Register("[job exception]", "An error just happened:" + Environment.NewLine + "{0}");
-            JobManager.JobException += (sender, e) => Log.This("[job exception]", e.ExceptionObject);
+            L.Register("[job exception]", "An error just happened:" + Environment.NewLine + "{0}");
+            JobManager.JobException += (sender, e) => L.Log("[job exception]", e.ExceptionObject);
         }
 
         private static void Initialize()
