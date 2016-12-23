2f2946d216fcfee1d0455d2d8732168260d5e6ac
Etienne Delattre <ejm.delattre@gmail.com>
2016-12-23 16:32:01 +0100

    Proposed feature Run on WeekDaysOnly also for Monthly unit

--- Library/Unit/MonthOnDayOfMonthUnit.cs
+++ Library/Unit/MonthOnDayOfMonthUnit.cs
@@ -1,9 +1,11 @@
 ﻿namespace FluentScheduler
 {
+    using System;
+
     /// <summary>
     /// Unit of time that represents a specific day of the month.
     /// </summary>
-    public sealed class MonthOnDayOfMonthUnit
+    public sealed class MonthOnDayOfMonthUnit : IDayRestrictableUnit
     {
         private readonly int _duration;
 
@@ -19,18 +21,26 @@
 
         internal Schedule Schedule { get; private set; }
 
+        Schedule IDayRestrictableUnit.Schedule { get { return this.Schedule; } }
+
+        public DateTime DayIncrement(DateTime increment)
+        {
+            return increment.AddDays(_duration);
+        }
+
         /// <summary>
         /// Runs the job at the given time of day.
         /// </summary>
         /// <param name="hours">The hours (0 through 23).</param>
         /// <param name="minutes">The minutes (0 through 59).</param>
-        public void At(int hours, int minutes)
+        public IDayRestrictableUnit At(int hours, int minutes)
         {
             Schedule.CalculateNextRun = x =>
             {
                 var nextRun = x.Date.First().AddDays(_dayOfMonth - 1).AddHours(hours).AddMinutes(minutes);
                 return x > nextRun ? x.Date.First().AddMonths(_duration).AddDays(_dayOfMonth - 1).AddHours(hours).AddMinutes(minutes) : nextRun;
             };
+            return this;
         }
     }
 }
