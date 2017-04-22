159d7297825b8ff6a96f09c24229005f56a1daff
Talles L <talleslasmar@gmail.com>
2017-04-22 19:24:24 -0300

    closes #131

--- Library/IJob.cs
+++ Library/IJob.cs
@@ -2,6 +2,8 @@
 {
     /// <summary>
     /// Some work to be done.
+    /// If you are relying on the library to instantiate the job, make sure you implement a parameterless constructor
+    /// (else you will be getting a System.MissingMethodException).
     /// </summary>
     public interface IJob
     {
