4652b183e5862765cada0177cf317285ab892a74
Talles L <talleslasmar@gmail.com>
2017-08-12 20:05:14 -0300

    Schedule class with ctor

---
+++ Library/Scheduler/Schedule.cs
@@ -0,0 +1,22 @@
+﻿namespace FluentScheduler
+{
+    using System;
+
+    /// <summary>
+    /// A job schedule.
+    /// </summary>
+    public class Schedule
+    {
+        private readonly Action _job;
+
+        /// <summary>
+        /// Creates a new schedule for the given job.
+        /// </summary>
+        /// <param name="job">Job to be scheduled</param>
+        /// <returns>A schedule for the given job</returns>
+        public Schedule(Action job)
+        {
+            _job = job;
+        }
+    }
+}
