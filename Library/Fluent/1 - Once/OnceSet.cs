06a2efb0bacc6dab2bf7bae9e498eb94f458994a
Talles L <talleslasmar@gmail.com>
2017-08-13 21:25:03 -0300

    Period scheduling and calculation.

---
+++ Library/Fluent/1 - Once/OnceSet.cs
@@ -0,0 +1,17 @@
+﻿namespace FluentScheduler
+{
+    public class OnceSet
+    {
+        private readonly TimeCalculator _calculator;
+
+        internal OnceSet(TimeCalculator calculator)
+        {
+            _calculator = calculator;
+        }
+
+        public PeriodDurationSet AndEvery(int duration)
+        {
+            return new PeriodDurationSet(duration, _calculator);
+        }
+    }
+}
--- Library/Scheduler/TimeCalculator.cs
+++ Library/Scheduler/TimeCalculator.cs
@@ -1,6 +1,8 @@
 ﻿namespace FluentScheduler
 {
     using System;
+    using System.Linq;
+    using System.Collections.Generic;
 
     internal class TimeCalculator
     {
@@ -8,9 +10,12 @@
 
         internal Func<DateTime, DateTime> OnceCalculation { get; set; }
 
+        internal IList<Func<DateTime, DateTime>> PeriodCalculations { get; private set; } =
+            new List<Func<DateTime, DateTime>>();
+
         public DateTime? Calculate(DateTime now)
         {
-            return CalculateOnce(now);
+            return CalculateOnce(now) ?? CalculatePeriod(now);
         }
 
         private DateTime? CalculateOnce(DateTime now)
@@ -20,6 +25,19 @@
 
             _firstCalculation = false;
             return OnceCalculation?.Invoke(now);
+        }
+
+        private DateTime? CalculatePeriod(DateTime now)
+        {
+            if (!PeriodCalculations.Any())
+                return null;
+
+            var next = now;
+
+            foreach (var calc in PeriodCalculations)
+                next = calc(next);
+
+            return next;
         }
     }
 }
