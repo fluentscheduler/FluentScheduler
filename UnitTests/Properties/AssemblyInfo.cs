4c6e9b15b4733f2fbd4d8a955f07dd6e9e302ab1
Talles L <talleslasmar@gmail.com>
2017-08-16 22:58:24 -0300

    Removed assembly signing.

--- UnitTests/Properties/AssemblyInfo.cs
+++ UnitTests/Properties/AssemblyInfo.cs
@@ -33,4 +33,3 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.0.0.0")]
 [assembly: AssemblyFileVersion("0.0.0.0")]
-[assembly:AssemblyKeyFileAttribute("..\\Library\\keyfile.snk")]
