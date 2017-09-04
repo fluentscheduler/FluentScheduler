7fe200b92525e7fa571081fea7f87b8812674469
Talles L <talleslasmar@gmail.com>
2017-09-03 22:11:10 -0300

    Some unit tests.

---
+++ UnitTests/PeriodDurationSetTests.cs
@@ -0,0 +1,56 @@
+﻿namespace FluentScheduler.UnitTests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+
+    [TestClass]
+    public class PeriodDurationSetTests
+    {
+        [TestMethod]
+        public void NowAndEvery()
+        {
+            // Arrange
+            var now = DateTime.Now;
+
+            var calculator = new TimeCalculator();
+            var run = new RunSpecifier(calculator);
+
+            // Act
+            run.Now().AndEvery(10).Seconds();
+
+            // Assert
+            DateTime? expected = now;
+            var actual = calculator.Calculate(now);
+
+            Assert.AreEqual(expected, actual);
+
+            actual = calculator.Calculate(now);
+            expected = expected.Value.AddSeconds(10);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void OnceAtAndEvery()
+        {
+            // Arrange
+            var now = new DateTime(2000, 10, 10, 10, 10, 10);
+            var calculator = new TimeCalculator();
+            var run = new RunSpecifier(calculator);
+
+            // Act
+            run.OnceAt(now).AndEvery(10).Seconds();
+
+            // Assert
+            DateTime? expected = now;
+            var actual = calculator.Calculate(DateTime.Now);
+
+            Assert.AreEqual(expected, actual);
+
+            expected = expected.Value.AddSeconds(10);
+            actual = calculator.Calculate(now);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ UnitTests/ScheduleTests.cs
@@ -0,0 +1,23 @@
+﻿namespace FluentScheduler.UnitTests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Threading;
+
+    [TestClass]
+    public class ScheduleTests
+    {
+        [TestMethod]
+        public void SimpleSchedule()
+        {
+            var expected = 2;
+            var actual = 0;
+
+            var schedule = new Schedule(() => ++actual, run => run.Every(1).Seconds());
+
+            schedule.Start();
+
+            Thread.Sleep(2100);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
