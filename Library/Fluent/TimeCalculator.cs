814a00d9dedac5f67ec7742d009350284a48233f
Talles L <talleslasmar@gmail.com>
2017-09-19 23:41:04 -0300

    Moving files around.

---
+++ Library/Fluent/TimeCalculator.cs
@@ -0,0 +1,43 @@
+﻿namespace FluentScheduler
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    internal class TimeCalculator
+    {
+        private bool _firstCalculation = true;
+
+        internal Func<DateTime, DateTime> OnceCalculation { get; set; }
+
+        internal IList<Func<DateTime, DateTime>> PeriodCalculations { get; private set; } =
+            new List<Func<DateTime, DateTime>>();
+
+        internal DateTime? Calculate(DateTime now)
+        {
+            return CalculateOnce(now) ?? CalculatePeriod(now);
+        }
+
+        private DateTime? CalculateOnce(DateTime now)
+        {
+            if (!_firstCalculation)
+                return null;
+
+            _firstCalculation = false;
+            return OnceCalculation?.Invoke(now);
+        }
+
+        private DateTime? CalculatePeriod(DateTime now)
+        {
+            if (!PeriodCalculations.Any())
+                return null;
+
+            var next = now;
+
+            foreach (var calc in PeriodCalculations)
+                next = calc(next);
+
+            return next;
+        }
+    }
+}
