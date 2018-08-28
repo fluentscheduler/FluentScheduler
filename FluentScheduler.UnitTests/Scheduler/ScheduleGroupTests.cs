f5b2e31472b639d4e8d0ea6273acec79d5400d47
VitorCioletti <vitor.morais@aec.com.br>
2018-08-28 19:57:40 -0300

    Added unit tests to NextRun, ListenJobStarted, UnlistenJobStarted, ListenJobEnded and UnlistenJobEnded

--- FluentScheduler.UnitTests/Scheduler/ScheduleGroupTests.cs
+++ FluentScheduler.UnitTests/Scheduler/ScheduleGroupTests.cs
@@ -189,5 +189,118 @@ namespace FluentScheduler.UnitTests
             // Assert
             Assert.AreEqual(expectedCalls, calls);
         }
+
+        [TestMethod]
+        public void UnlistenJobStarted()
+        {
+            // Arrange
+            var calls = 0;
+            var expectedCalls = 2;
+
+            EventHandler<JobStartedEventArgs> jobStartedEvent = (_, e) => calls++; 
+
+            var scheduleGroup = new List<Schedule>
+            {
+                new Schedule(() => { } , run => run.Now()),
+                new Schedule(() => { } , run => run.Now()),
+            };
+
+            // Act
+            scheduleGroup.ListenJobStarted(jobStartedEvent);
+            scheduleGroup.Start();
+
+            Thread.Sleep(100);
+
+            scheduleGroup.StopAndBlock();
+            scheduleGroup.ResetScheduling();
+
+            scheduleGroup.UnlistenJobStarted(jobStartedEvent);
+
+            scheduleGroup.Start();
+
+            Thread.Sleep(100);
+
+            // Assert
+            Assert.AreEqual(expectedCalls, calls);
+        }
+
+        [TestMethod]
+        public void ListenJobEnded()
+        {
+            // Arrange
+            var calls = 0;
+            var expectedCalls = 2;
+
+            var scheduleGroup = new List<Schedule>
+            {
+                new Schedule(() => { } , run => run.Now()),
+                new Schedule(() => { } , run => run.Now()),
+            };
+
+            // Act
+            scheduleGroup.ListenJobEnded((_, e) => calls++);
+            scheduleGroup.Start();
+
+            Thread.Sleep(100);
+
+            // Assert
+            Assert.AreEqual(expectedCalls, calls);
+        }
+
+        [TestMethod]
+        public void UnlistenJobEnded()
+        {
+            // Arrange
+            var calls = 0;
+            var expectedCalls = 2;
+
+            EventHandler<JobEndedEventArgs> jobEndedEvent = (_, e) => calls++; 
+
+            var scheduleGroup = new List<Schedule>
+            {
+                new Schedule(() => { } , run => run.Now()),
+                new Schedule(() => { } , run => run.Now()),
+            };
+
+            // Act
+            scheduleGroup.ListenJobEnded(jobEndedEvent);
+            scheduleGroup.Start();
+
+            Thread.Sleep(100);
+
+            scheduleGroup.StopAndBlock();
+            scheduleGroup.ResetScheduling();
+
+            scheduleGroup.UnlistenJobEnded(jobEndedEvent);
+
+            scheduleGroup.Start();
+
+            Thread.Sleep(100);
+
+            // Assert
+            Assert.AreEqual(expectedCalls, calls);
+        }
+
+        [TestMethod]
+        public void NextRun()
+        {
+            // Arrange
+            var scheduleGroup = new List<Schedule>
+            {
+                new Schedule(() => { } , run => run.Every(10).Minutes()),
+                new Schedule(() => { } , run => run.Every(20).Minutes()),
+            };
+
+            // Act
+            scheduleGroup.Start();
+            var expectedNextRun = DateTime.Now;
+
+            Thread.Sleep(100);
+
+            var nextRun = scheduleGroup.NextRun();
+
+            // Assert
+            Assert.AreEqual(expectedNextRun.AddMinutes(10).Minute, nextRun.Value.Item2.Minute);
+        }
     }
 }
