e4a610ac366ddc384ed889fde884417ac5912b7d
VitorCioletti <ciolettiv@gmail.com>
2018-12-29 16:32:09 -0200

    Fixed issue #198

--- FluentScheduler/Fluent/3 - Duration/PeriodOnceSet.cs
+++ FluentScheduler/Fluent/3 - Duration/PeriodOnceSet.cs
@@ -22,7 +22,14 @@
                 throw new ArgumentOutOfRangeException($"\"{nameof(minute)}\" should be in the 0 to 59 range.");
 
             _calculator.PeriodCalculations.Add(last =>
-                new DateTime(last.Year, last.Month, last.Day, hour, minute, 0));
+            {
+                var now = DateTime.Now;
+                var next = new DateTime(last.Year, last.Month, last.Day, hour, minute, 0);
+
+                return now.TimeOfDay < next.TimeOfDay ?
+                    new DateTime(now.Year, now.Month, now.Day, hour, minute, 0) :
+                    next;
+            });
         }
 
         /// <summary>
