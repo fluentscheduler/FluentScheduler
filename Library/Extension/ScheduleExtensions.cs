46838585d6837ee34f088a4681ec3f23a7519eed
Dave Higgins <41444963+dave-yotta@users.noreply.github.com>
2019-01-29 17:14:33 +0000

    Find the next run of a schedule given a start date

---
+++ Library/Extension/ScheduleExtensions.cs
@@ -0,0 +1,33 @@
+namespace FluentScheduler
+{
+    using System;
+
+    internal static class ScheduleExtensions
+    {
+        public static DateTime? FindNextRun(this Schedule schedule, DateTime following)
+        {
+            var next = schedule.CalculateNextRun(following);
+
+            if (next > following)
+            {
+                return next;
+            }
+            else
+            {
+                var subNext = schedule.AdditionalSchedules
+                                      .Select(x => x.FindNextRun(following))
+                                      .Where(x => x.HasValue)
+                                      .OrderBy(x => x.Value)
+                                      .Where(x => x.Value > following)
+                                      .ToArray();
+
+                if (subNext.Length > 0)
+                {
+                    return subNext.First();
+                }
+            }
+
+            return null;
+        }
+    }
+}
