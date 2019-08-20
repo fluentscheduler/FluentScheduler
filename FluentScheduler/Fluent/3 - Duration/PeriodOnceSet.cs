62ad2ecffd043d43720241e00ddfe697c686efd9
VitorCioletti <ciolettiv@gmail.com>
2019-08-20 13:54:41 -0300

    Fixed incosistency in Between logic.

--- FluentScheduler/Fluent/3 - Duration/PeriodOnceSet.cs
+++ FluentScheduler/Fluent/3 - Duration/PeriodOnceSet.cs
@@ -46,6 +46,16 @@
         /// <summary>
         /// Runs the job at the given period of time.
         /// </summary>
+        /// <param name="hourFrom">Hour to delimitate period beginning.</param>
+        /// <param name="minuteFrom">Minute to delimitade period beginning.</param>
+        /// <param name="hourTo">Hour to delimitate period end.</param>
+        /// <param name="minuteTo">Minute to delimitade period end.</param>
+        public void Between(int hourFrom, int minuteFrom, int hourTo, int minuteTo) =>
+            Between(new TimeSpan(hourFrom, minuteFrom, 0), new TimeSpan(hourTo, minuteTo, 0));
+
+        /// <summary>
+        /// Runs the job at the given period of time.
+        /// </summary>
         /// <param name="from">Time of the day to delimitate the period beginnig.</param>
         /// <param name="to">Time of the day to delimitade the period end.</param>
         public void Between(TimeSpan from, TimeSpan to)
@@ -57,6 +67,9 @@
 
                     var next = new DateTime(last.Year, last.Month, last.Day).Add(from);
 
+                    if (from < to)
+                        return  next;
+                
                     if (lastTime < from)
                         return next;
 
