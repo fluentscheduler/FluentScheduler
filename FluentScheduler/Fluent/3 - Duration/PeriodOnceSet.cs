8baaf86b9c97cbe9d5fa2a518ab584902980f328
VitorCioletti <ciolettiv@gmail.com>
2019-08-20 20:35:06 -0300

    Refactored between method

--- FluentScheduler/Fluent/3 - Duration/PeriodOnceSet.cs
+++ FluentScheduler/Fluent/3 - Duration/PeriodOnceSet.cs
@@ -67,11 +67,8 @@
 
                     var next = new DateTime(last.Year, last.Month, last.Day).Add(from);
 
-                    if (from >= to)
+                    if (from >= to || lastTime < from)
                         return  next;
-                
-                    if (lastTime < from)
-                        return next;
 
                     if (lastTime > to)
                         return next.AddDays(1);
