834e0a1d280a43a84fd9ffc849c20deeac625dc8
VitorCioletti <ciolettiv@gmail.com>
2020-04-30 00:19:04 -0300

    Removed OnTheFirst/Second/Third/Fourth functions.

--- FluentScheduler/Fluent/3 - Duration/Month/MonthUnit.cs
+++ FluentScheduler/Fluent/3 - Duration/Month/MonthUnit.cs
@@ -20,51 +20,5 @@
 
             return new RestrictionUnit(_calculator);
         }
-
-        /// <summary>
-        /// Runs the job on the given day of week on the first week of the month.
-        /// </summary>
-        /// <param name="day">The day of the week</param>
-        public RestrictionUnit OnTheFirstDay(DayOfWeek day)
-        {
-            _calculator.PeriodCalculations.Add(last => SelectNthDay(last.Date.Year, last.Date.Month, day, 1));
-            return new RestrictionUnit(_calculator);
-        }
-
-        /// <summary>
-        /// Runs the job on the given day of week on the second week of the month.
-        /// </summary>
-        /// <param name="day">The day of the week</param>
-        public RestrictionUnit OnTheSecondDay(DayOfWeek day)
-        {
-            _calculator.PeriodCalculations.Add(last => SelectNthDay(last.Date.Year, last.Date.Month, day, 2));
-            return new RestrictionUnit(_calculator);
-        }
-
-        /// <summary>
-        /// Runs the job on the given day of week on the third week of the month.
-        /// </summary>
-        /// <param name="day">The day of the week</param>
-        public RestrictionUnit OnTheThirdDay(DayOfWeek day)
-        {
-            _calculator.PeriodCalculations.Add(last => SelectNthDay(last.Date.Year, last.Date.Month, day, 3));
-            return new RestrictionUnit(_calculator);
-        }
-
-        /// <summary>
-        /// Runs the job on the given day of week on the fourth week of the month.
-        /// </summary>
-        /// <param name="day">The day of the week</param>
-        public RestrictionUnit OnTheFourthDay(DayOfWeek day)
-        {
-            _calculator.PeriodCalculations.Add(last => SelectNthDay(last.Date.Year, last.Date.Month, day, 4));
-            return new RestrictionUnit(_calculator);
-        }
-
-        private static DateTime SelectNthDay(int year, int month, DayOfWeek dayOfWeek, int occurrence) =>
-            Enumerable.Range(1, 7)
-                .Select(day => new DateTime(year, month, day))
-                .First(dateTime => dateTime.DayOfWeek == dayOfWeek)
-                .AddDays(7 * (occurrence - 1));
     }
 }
